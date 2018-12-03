893f9f6347975172f7049c53fc37ab96d63709ef
Prakash Koneru <sivaprakashkoneru67@gmail.com>
2018-12-03 02:47:04 -0500

    Logic for making API calls and rendering Data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { fetchImagesAction } from './actions.creators';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 class App extends Component {
   state = {
@@ -19,6 +20,19 @@ class App extends Component {
     const { images } = this.props;
     return (
       <div className="App">
+        {images && images.map((image) => {
+          const imageTakenOn = moment(image.date).format('YYYY/MM/DD');
+          return (
+            <div key={image.identifier}>
+              <img
+                src={`https://epic.gsfc.nasa.gov/archive/natural/${imageTakenOn}/png/epic_1b_${image.identifier}.png`}
+                alt="Image not Found"
+                height="300px"
+                width="300px"
+              />
+            </div>
+          )
+        })}
         <input type="date" onChange={this.inputChange}/>
         <button onClick={this.fetchData}>Get Picture</button>
       </div>
--- src/sagas.js
+++ src/sagas.js
@@ -1,8 +1,25 @@
-import { takeEvery, all } from 'redux-saga/effects'
-import { fetchImages } from './actions.types';
+import { takeEvery, all, call, put } from 'redux-saga/effects'
+import { fetchImages, updateImagesData } from './actions.types';
+import axios from 'axios';
+import moment from 'moment';
+
+const submitDate = (date) => axios.get(`https://epic.gsfc.nasa.gov/api/enhanced/date/${date}`);
 
 function* fetchImagesData({ type, payload }) {
-  yield console.log('here would be the actual logic to perform API calls');
+  try {
+    let closestBirthDate = payload;
+    while (true) {
+      const { data } = yield call(submitDate, moment(closestBirthDate).format('YYYY-MM-DD'));
+      if (data.length > 0) {
+        yield put({ type: updateImagesData, payload: data })
+        break;
+      } else {
+        closestBirthDate = moment(closestBirthDate).add(1,'days').format('YYYY-MM-DD');
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default function* rootSaga() {
